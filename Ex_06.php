a317191d63af6f6f0d94b742a0dddb7d582bfbcb
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-08-06 11:00:31 -0300

    ADD - Exercício 06: Conversor de Temperatura

---
+++ Ex_06.php
@@ -0,0 +1,30 @@
+<?php
+
+// Exercício 06 - Conversor de Temperatura
+
+function converterTemperatura(float $valor, string $origem, string $destino): float {
+    $origem = strtupper($origem);
+    $destino = strtupper($destino);
+
+    $celsius = match ($origem) {
+        'C' => $valor,
+        'F' => ($valor - 32) * 5 / 9,
+        'K' => $valor - 273.15,
+        default => throw new InvalidArgumentException("Escala de origem inválida")
+    };
+
+    return match ($destino) {
+        'C' => $celsius,
+        'F' => ($celsius * 9 / 5) + 32,
+        'K' => $celsius + 273.15,
+        default => throw new InvalidArgumentException("Escala de destino inválida")
+    };
+}
+
+$valor = 50;
+$origem = 'F';
+$destino = 'C';
+
+echo converterTemperatura($valor, $origem, $destino);
+
+?>
