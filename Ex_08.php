e82737bf22f3f580a0f0ddef7a8512f6980c4d77
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-08-06 11:17:12 -0300

    ADD - Ajustei o Exercício 08

--- Ex_08.php
+++ Ex_08.php
@@ -2,4 +2,22 @@
 
 //Exercício 08 - Organizador de Lista
 
+function ordenarNomes($nomes) {
+    $vetor = explode(", ", $nomes);
+
+    foreach ($vetor as $nome) {
+        $nome = trim($nome);
+    }
+
+    sort($vetor);
+
+    return implode(", ", $vetor);
+
+}
+
+$nomes = "Rafaella, João, Riquelme, Higor, Bernardo";
+
+echo "Nomes desordenados: " . ordenarNomes($nomes);
+echo "Nomes ordernados: " . ordenarNomes($nomes) . "<br>";
+
 ?>
