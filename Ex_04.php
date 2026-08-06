75ed4e9bfbd0859187ab2e8c329b2d91c5098904
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-08-06 08:23:26 -0300

    ADD - Exercício 04: Gerador de Senhas

--- Ex_04.php
+++ Ex_04.php
@@ -2,5 +2,23 @@
 
 // Exercício 04 - Gerador de Senhas
 
+function gerarSenha($n) {
+    $maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $minusculas = "abcdefghijklmnopqrstuvwxyz";
+    $numeros = "0123456789";
+    $caracteresEspeciais = "!@#$%^&*()_+-=[]{}";
+
+    $todosCaracteres = $maiusculas . $minusculas . $numeros . $caracteresEspeciais;
+
+    $senha = "";
+    for ($i = 0; $i < $n; $i++) {
+        $senha .= $todosCaracteres[mt_rand(0, strlen($todosCaracteres) - 1)];
+}
+
+    return $senha;
+}
+
+$n = 7;
+echo "Senha gerada: " . gerarSenha($n);
 
 ?>
