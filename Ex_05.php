7e85c85e1f81cdbf9bd87f5345a9fa48574bf4a7
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-08-06 08:27:50 -0300

    ADD - Exercício 05: Estatísticas de Texto

--- Ex_05.php
+++ Ex_05.php
@@ -2,4 +2,19 @@
 
 // Exercício 05 - Estatísticas de Texto
 
+function analisarTexto($texto) {
+    $quantidadePalavras = str_word_count($texto);
+    $quantidadeCaracteres = strlen(str_replace(' ', '', $texto));
+    $quantidadeVogais = preg_match_all('/[aeiouAEIOU]/', $texto);
+    $quantidadeConsoantes = preg_match_all('/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/', $texto);
+
+    echo "Quantidade de palavras: " . $quantidadePalavras . "<br>";
+    echo "Quantidade de caracteres: " . $quantidadeCaracteres . "<br>";
+    echo "Quantidade de vogais: " . $quantidadeVogais . "<br>";
+    echo "Quantidade de consoantes: " . $quantidadeConsoantes . "<br>";
+}
+
+$texto = "Hoje de manhã, fui ao supermercado e comprei frutas, legumes e pão.";
+analisarTexto($texto);
+
 ?>
